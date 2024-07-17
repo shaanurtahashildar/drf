a31d8d4bcfe97a6b7f95522f7eb92736d3e03585
TEROX <shahidtashildar@gmail.com>
2024-07-17 23:39:04 +0530

    custom permissions complete

--- products/permissions.py
+++ products/permissions.py
@@ -1,25 +1,36 @@
 from rest_framework import permissions
 
 class IsStaffEditoPermissions(permissions.DjangoModelPermissions):
-    def has_permission(self, request, view):
-        if request.user.is_staff():
-            return False
-        return super().has_permission(request, view)
+
     def has_permission(self, request, view):#app_name.verb_model_name
+        perms_map = {
+            'GET': ['%(app_label)s.view_%(model_name)s'],
+            'OPTIONS': [],
+            'HEAD': [],
+            'POST': ['%(app_label)s.add_%(model_name)s'],
+            'PUT': ['%(app_label)s.change_%(model_name)s'],
+            'PATCH': ['%(app_label)s.change_%(model_name)s'],
+            'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        }
+
         user=request.user
         print(user.get_all_permissions())
-        if user.is_staff:
-            if user.has_perm("product.add_product"):
-                return True
-            if user.has_perm("product.change_product"):
-                return True
-            if user.has_perm("product.delete_product"):
-                return True
-            if user.has_perm("product.view_product"):
-                return True
+        if not user.is_staff: #admin has permissions
             return False
 
-        return False
+        return super().has_permission(request, view)
+        # if user.is_staff:
+        #     if user.has_perm("products.add_product"):
+        #         return True
+        #     if user.has_perm("products.change_product"):
+        #         return True
+        #     if user.has_perm("products.delete_product"):
+        #         return True
+        #     if user.has_perm("products.view_product"):
+        #         return True
+        #     return False
+        #
+        # return False
 
     # def has_object_permission(self, request, view, obj):
     #     return True
